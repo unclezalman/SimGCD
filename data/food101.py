ec2c245acbceb757ec41527e5b2a6f9f7a04a5ab
unclezalman <160236291+unclezalman@users.noreply.github.com>
2025-04-27 11:21:14 +0800

    Update food101.py

--- data/food101.py
+++ data/food101.py
@@ -1,35 +1,36 @@
 import os
-import torchvision
 import numpy as np
 from copy import deepcopy
-
-from data.data_utils import subsample_instances
-from config import food101_root
 from torchvision.datasets import Food101
+from data.data_utils import subsample_instances
 
 class CustomFood101(Food101):
     def __init__(self, *args, **kwargs):
         split = kwargs.pop('split', 'train')
         super().__init__(*args, split=split, **kwargs)
         self.uq_idxs = np.array(range(len(self)))
-        
+        self.targets = self._labels  
+    
     def __getitem__(self, idx):
         img, target = super().__getitem__(idx)
         return img, target, self.uq_idxs[idx]
+    
+    def __len__(self):
+        return len(self._labels)
 
 def subsample_dataset(dataset, idxs):
     if len(idxs) == 0:
         return None
         
-    dataset.data = [dataset.data[i] for i in idxs]
-    dataset._labels = [dataset._labels[i] for i in idxs]
-    dataset.uq_idxs = dataset.uq_idxs[idxs]
-    return dataset
+    new_dataset = deepcopy(dataset)
+    new_dataset._labels = [dataset._labels[i] for i in idxs]
+    new_dataset._image_files = [dataset._image_files[i] for i in idxs]
+    new_dataset.uq_idxs = dataset.uq_idxs[idxs]
+    return new_dataset
 
 def subsample_classes(dataset, include_classes):
     cls_idxs = [i for i, label in enumerate(dataset._labels) if label in include_classes]
     return subsample_dataset(dataset, cls_idxs)
-
 
 def get_food101_datasets(train_transform, test_transform, train_classes=range(50),
                         prop_train_labels=0.8, split_train_val=False, seed=0):
@@ -40,16 +41,14 @@ def get_food101_datasets(train_transform, test_transform, train_classes=range(50
     test_dataset = CustomFood101(root=food101_root, split='test',
                                transform=test_transform, download=True)
 
-
     train_dataset_labelled = subsample_classes(deepcopy(whole_train), train_classes)
     subsample_indices = subsample_instances(train_dataset_labelled, prop_train_labels)
     train_dataset_labelled = subsample_dataset(train_dataset_labelled, subsample_indices)
 
-
-    labelled_idxs = set(train_dataset_labelled.uq_idxs)
-    unlabelled_idxs = [i for i in range(len(whole_train)) if i not in labelled_idxs]
+    labelled_idxs = set(range(len(train_dataset_labelled)))
+    all_train_idxs = set(range(len(whole_train)))
+    unlabelled_idxs = list(all_train_idxs - labelled_idxs)
     train_dataset_unlabelled = subsample_dataset(deepcopy(whole_train), unlabelled_idxs)
-
 
     all_classes = list(train_classes) + list(set(range(101)) - set(train_classes))
     target_xform = {cls:i for i, cls in enumerate(all_classes)}
